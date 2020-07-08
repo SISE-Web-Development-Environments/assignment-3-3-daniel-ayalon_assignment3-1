19b5a3085a82f2b27b0529517909fc8661a04fcb
Daniel Kolbasov <kolbasov@post.bgu.ac.il>
2020-07-08 15:15:55 +0300

    updateLocalHost

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -94,7 +94,7 @@ export default {
     async Login() {
       try {
         const response = await this.axios.post(
-          "https://test-for-3-2.herokuapp.com/user/Login",
+          "http://localhost:3000/auth/Login",
           {
             username: this.form.username,
             password: this.form.password
