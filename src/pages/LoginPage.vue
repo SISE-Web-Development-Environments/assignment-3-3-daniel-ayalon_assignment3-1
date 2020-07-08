084ba092733afc05d743a14bb9e17772174b4048
Daniel Kolbasov <kolbasov@post.bgu.ac.il>
2020-07-08 16:02:07 +0300

    updateCookies

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -98,7 +98,8 @@ export default {
           {
             username: this.form.username,
             password: this.form.password
-          }
+          },
+          {withCredentials: true}
         );
         // console.log(response);
         // this.$root.loggedIn = true;
